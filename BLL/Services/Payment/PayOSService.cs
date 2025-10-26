057063b68b0b16575ad8778359b587135bf33ff8
izasterisk <nguyenngochoa12112003@gmail.com>
2025-10-26 12:36:15 +0700

    fix: Refactor PayOS payment DTOs and service logic
    Replaces PayOSPaymentDTO.cs with PayOSPaymentCreateDTO.cs, introducing new DTOs with validation attributes for payment creation. Updates PayOSService to use the new DTO and refactors CreatePaymentLinkAsync to fetch order data and map DTOs accordingly. Removes unused DTOs and aligns service logic with updated data structures.

--- BLL/Services/Payment/PayOSService.cs
+++ BLL/Services/Payment/PayOSService.cs
@@ -1,14 +1,48 @@
-﻿using BLL.Interfaces;
+﻿using BLL.DTO.Payment.PayOS;
+using BLL.Interfaces;
 using BLL.Interfaces.Infrastructure;
+using DAL.Data;
+using DAL.IRepository;
+using Net.payOS.Types;
 
 namespace BLL.Services.Payment;
 
 public class PayOSService : IPayOSService
 {
     private readonly IPayOSApiClient _payOSApiClient;
-
-    public PayOSService(IPayOSApiClient PayOSApiClient)
+    private readonly IOrderRepository _orderRepository;
+    
+    public PayOSService(IPayOSApiClient payOSApiClient, IOrderRepository orderRepository)
     {
-        _payOSApiClient = PayOSApiClient;
+        _payOSApiClient = payOSApiClient;
+        _orderRepository = orderRepository;
+    }
+    
+    public async Task<CreatePaymentResult> CreatePaymentLinkAsync(ulong orderId, CreatePaymentDataDTO dto)
+    {
+        var order = await _orderRepository.GetOrderByIdAsync(orderId);
+        if (order == null || order.Status != OrderStatus.Paid)
+        {
+            throw new ArgumentException($"Đơn hàng với ID {orderId} không tồn tại hoặc không khả dụng để thanh toán.");
+        }
+        PaymentData paymentData = new PaymentData(
+            orderCode: checked((long)order.Id),
+            amount: (int)Math.Round(order.TotalAmount, MidpointRounding.AwayFromZero),
+            description: dto.Description,
+            items: dto.Items.Select(item => new ItemData(
+                name: item.Name,
+                quantity: item.Quantity,
+                price: item.Price
+            )).ToList(),
+            cancelUrl: dto.CancelUrl,
+            returnUrl: dto.ReturnUrl,
+            signature: dto.Signature,
+            buyerName: dto.BuyerName,
+            buyerEmail: dto.BuyerEmail,
+            buyerPhone: dto.BuyerPhone,
+            buyerAddress: dto.BuyerAddress,
+            expiredAt: dto.ExpiredAt
+        );
+        return await _payOSApiClient.CreatePaymentLinkAsync(paymentData);
     }
 }
