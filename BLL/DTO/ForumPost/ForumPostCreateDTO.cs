bb06f41e2672833b48e92d122a6b6442d82a67b2
Trung Nguyên Nguyễn <125417382+Legen25@users.noreply.github.com>
2025-11-16 10:12:04 +0700

    refactor: update function, fix merge Forum Post(paging)

---
+++ BLL/DTO/ForumPost/ForumPostCreateDTO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO.ForumPost
+{
+    public class ForumPostCreateDTO
+    {
+        public ulong ForumCategoryId { get; set; }
+
+        public string Title { get; set; } = null!;
+        public string Slug { get; set; } = null!;
+        public string? Tags { get; set; }
+
+        // Content blocks
+        public List<ContentBlockDTO> Content { get; set; } = new();
+
+        // Upload nhiều ảnh
+        public IEnumerable<IFormFile>? AddImages { get; set; }
+    }
+}
