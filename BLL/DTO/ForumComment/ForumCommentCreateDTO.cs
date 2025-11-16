449027c460a1170063360cf7e4e05c32464ea6bb
Trung Nguyên Nguyễn <125417382+Legen25@users.noreply.github.com>
2025-11-16 10:11:21 +0700

    refactor: update function, fix merge Forum Comment(comment with parentid, notificate, paging)

---
+++ BLL/DTO/ForumComment/ForumCommentCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO.ForumComment
+{
+    public class ForumCommentCreateDTO
+    {
+        public ulong ForumPostId { get; set; }
+        public ulong UserId { get; set; }
+        public ulong? ParentId { get; set; }
+        public string Content { get; set; } = null!;
+    }
+}
---
+++ BLL/Interfaces/IForumCommentService.cs
@@ -0,0 +1,23 @@
+﻿using BLL.DTO.ForumComment;
+using DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IForumCommentService
+    {
+        Task<IEnumerable<ForumCommentResponseDTO>> GetCommentsByPostIdAsync( ulong postId, int page, int pageSize, CancellationToken ct = default);
+        Task<ForumCommentResponseDTO?> GetDetailAsync( ulong id, CancellationToken ct = default);
+        Task<ForumCommentResponseDTO> CreateAsync( ForumCommentCreateDTO dto,CancellationToken ct = default);
+        Task<ForumCommentResponseDTO> UpdateAsync( ForumCommentUpdateDTO dto, CancellationToken ct = default);
+        Task DeleteAsync(ulong id, CancellationToken ct = default);
+        Task ChangeStatusAsync(ulong id, ForumCommentStatus status, CancellationToken ct = default);
+        Task IncrementLikeAsync(ulong id, CancellationToken ct = default);
+        Task IncrementDislikeAsync(ulong id, CancellationToken ct = default);
+    }
+
+}
